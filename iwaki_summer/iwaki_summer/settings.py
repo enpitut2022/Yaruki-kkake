9d8153dc872fac44af2c3e70c4bd05e7797c2386
tatsuji_ho <longer271271@gmail.com>
2022-07-25 17:27:14 +0900

    update settings.py
    staticのpathをtemplatesに変更

--- iwaki_summer/iwaki_summer/settings.py
+++ iwaki_summer/iwaki_summer/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/templates/'
 STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'static'),
+ os.path.join(BASE_DIR, 'templates'),
 )
 
 # Default primary key field type
