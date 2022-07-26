f83ea81fc35c09511d7c22b89b643ec284448e7f
tatsuji_ho <longer271271@gmail.com>
2022-07-26 10:33:19 +0900

    空白の start.html を追加

--- iwaki_summer/yaruki_kkake/urls.py
+++ iwaki_summer/yaruki_kkake/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index 
+from .views import index, start
 
 urlpatterns = [
   path("", index),
+  path("start", start),
 ]
--- iwaki_summer/yaruki_kkake/views.py
+++ iwaki_summer/yaruki_kkake/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'yaruki_kkake/index.html')
+
+def start(request):
+    return render(request, 'yaruki_kkake/start.html')
