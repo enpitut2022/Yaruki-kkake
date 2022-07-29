819c3e6b84b2793c16d5466cfec03bf3e27ed1bf
tatsuji_ho <longer271271@gmail.com>
2022-07-29 10:23:49 +0900

    ユーザー名を入力して反映できるようになりました

--- iwaki_summer/yaruki_kkake/views.py
+++ iwaki_summer/yaruki_kkake/views.py
@@ -5,7 +5,10 @@ def index(request):
     return render(request, 'yaruki_kkake/index.html')
     
 def start(request):
-    return render(request, 'yaruki_kkake/start.html')
+    user_name = {
+        "user_name" : request.GET.get("user_name")
+    }
+    return render(request, 'yaruki_kkake/start.html', user_name)
 
 def start2(request):
     return render(request, 'yaruki_kkake/start2.html')
