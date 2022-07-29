b6c19acc291061a9bae83c0df19662d532fba692
tatsuji_ho <longer271271@gmail.com>
2022-07-29 11:39:17 +0900

    ユーザー名がどのページでも表示されるようになりました

--- iwaki_summer/yaruki_kkake/views.py
+++ iwaki_summer/yaruki_kkake/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def index(request):
@@ -8,17 +9,21 @@ def start(request):
     user_name = {
         "user_name" : request.GET.get("user_name")
     }
+    user = User(name=user_name["user_name"])
+    user.save()
+    request.session['user_name'] = user_name["user_name"]
     return render(request, 'yaruki_kkake/start.html', user_name)
 
 def start2(request):
     return render(request, 'yaruki_kkake/start2.html')
 
 def doing(request):
-    time = {
-        "time" : request.GET.get("time")
+    context = {
+        "time" : request.GET.get("time"),
+        "user_name" : request.session['user_name']
     }
     print(request.GET)
-    return render(request, 'yaruki_kkake/doing.html', time)
+    return render(request, 'yaruki_kkake/doing.html', context)
 
 def doing2(request):
     return render(request, 'yaruki_kkake/doing2.html')
